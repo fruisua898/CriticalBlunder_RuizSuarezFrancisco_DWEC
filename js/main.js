5b8abd48d0bf31c2c0334910715ab33311b99e98
Frajs <fruiz.alumno@iesrafaelalberti.es>
2021-11-30 16:57:15 +0100

    WIP VADEMECUM
    TODO
    - Vademecum grid @media
    - Boton (Formulario)
    - El footer, pequeño repaso.
    - Definir selectores del formulario de manera más eficaz.
    
    MEJORAS
    
    - Tabla lista y responsive
    - Estructura javascript para operar con API terminada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Overlay asincrono para la estructura de equipamiento */
+
 function eleccion_equipo() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
@@ -6,6 +8,9 @@ function eleccion_equipo() {
     xhttp.open("GET", "items.txt", true);
     xhttp.send();
 }
+
+
+/* Overlay asincrono para la estructura de monstruos */
 
 function monstruos() {
     const xhttp = new XMLHttpRequest();
@@ -17,9 +22,7 @@ function monstruos() {
 
 }
 
-
-
-
+/* Función que imprime los monstruos en pantalla */
 
 function mostrar_monstruos(data){
     let tabla = document.getElementById("escribir-monstruos");
@@ -33,33 +36,49 @@ function mostrar_monstruos(data){
         var td = document.createElement("td");
         td.innerHTML = data.results[i].name;
         tabla.appendChild(td);
+        td.id = data.results[i].name;
         var td = document.createElement("td");
         td.innerHTML = data.results[i].type;
+        td.classList.add(data.results[i].type);
         tabla.appendChild(td);
         var td = document.createElement("td");
         td.innerHTML = data.results[i].challenge_rating;
         tabla.appendChild(td);
-
     }
+}
 
+/*Busquedas*/
 
+/* --- FILTRO DE BUSQUEDA - MONSTRUOS  --- */
+
+function busqueda_monstruo() {
+    let src = document.getElementById("monster-campaign-values").value;
+    let name = document.getElementById("monster-campaign-name").value;
+    monstruos();
+    fetch('https://api.open5e.com/monsters/?search='+name+'&document__slug='+src)
+        .then(monstruos => monstruos.json())
+        .then(data => mostrar_monstruos(data));
+    monstruos(); //Si no recibimos nada muestra el sitio donde estamos.
 }
 
 
+
+
+
 function recoleccion_monstruos_nombre() {
-    monstruos()
+    monstruos();
     fetch('https://api.open5e.com/monsters/?limit=555&ordering=name')
         .then(monstruos => monstruos.json())
         .then(data => mostrar_monstruos(data));
 }
 function recoleccion_monstruos_type() {
-    monstruos()
+    monstruos();
     fetch('https://api.open5e.com/monsters/?limit=555&ordering=type')
         .then(monstruos => monstruos.json())
         .then(data => mostrar_monstruos(data));
 }
 function recoleccion_monstruos_cr() {
-    monstruos()
+    monstruos();
     fetch('https://api.open5e.com/monsters/?limit=555&ordering=challenge_rating')
         .then(monstruos => monstruos.json())
         .then(data => mostrar_monstruos(data));
