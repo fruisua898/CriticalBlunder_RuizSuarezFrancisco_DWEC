0969f70b5d6622beec8e80e2f78060e1ccf91092
Frajs <fruiz.alumno@iesrafaelalberti.es>
2021-12-01 21:17:49 +0100

    WIP VADEMECUM
    TODO
    
    - Rellenar bien el resto de la tabla.
    
    MEJORAS
    
    - Creada generación de tabla

--- js/main.js
+++ js/main.js
@@ -19,8 +19,17 @@ function monstruos() {
     }
     xhttp.open("GET", "monsters.txt", true);
     xhttp.send();
-
 }
+
+function monstruos_detail() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById("grid-main--vademecum").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "detalle-monstruo.txt", true);
+    xhttp.send();
+}
+
 
 /* Función que imprime los monstruos en pantalla */
 
@@ -37,6 +46,9 @@ function mostrar_monstruos(data){
         tabla.appendChild(tr);
         var td = document.createElement("td");
         td.innerHTML = data.results[i].name;
+        td.onclick = function () {                      //Con esto le pasamos la ID del bicho a otra función
+            detalle_monstruo(data.results[i].name,data.results[i].challenge_rating);     //que se encarga de mostrarlo por pantalla*/
+        }
         tr.appendChild(td);
         td.id = data.results[i].name;
         var td = document.createElement("td");
@@ -90,6 +102,7 @@ function recoleccion_monstruos_nombre() {
         .then(monstruos => monstruos.json())
         .then(data => mostrar_monstruos(data));
 }
+
 function recoleccion_monstruos_type() {
     monstruos();
     fetch('https://api.open5e.com/monsters/?limit=1086&ordering=type')
@@ -102,3 +115,208 @@ function recoleccion_monstruos_cr() {
         .then(monstruos => monstruos.json())
         .then(data => mostrar_monstruos(data));
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function detalle_monstruo(monstruo,cr) {
+    monstruos_detail();
+    fetch('https://api.open5e.com/monsters/?search='+monstruo+'&challenge_rating='+cr)
+        .then(monstruo => monstruo.json())
+        .then(monster => mostrar_detalle(monster));
+}
+function mostrar_detalle(monster) {
+    let lateral = document.getElementById("detalle-monstruo-print");
+    for (let i = 0; i < monster.results.length; i++) {
+
+        var tabla = document.createElement("table");
+        tabla.className="tabla-detalles";
+        lateral.appendChild(tabla);
+
+        var tr = document.createElement("tr");
+        tr.className="nombre";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML = monster.results[i].name;
+        tr.appendChild(td);
+
+        var tr = document.createElement("tr");
+        tr.className="tipos";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML = monster.results[i].size+" "+monster.results[i].type+", "+" "+monster.results[i].alignment;
+        if (monster.results[i].subtype){
+            td.innerHTML = monster.results[i].size+" "+monster.results[i].type+", "+"( "+monster.results[i].subtype+" )"+" "+monster.results[i].alignment;
+            tr.appendChild(td);
+        }
+        td.colSpan=2;
+        td.className="rayote";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.rowSpan=2;
+        tr.appendChild(td);
+        var img = document.createElement("img");
+        img.className="avatar-detalle-monstruo";
+        img.src = 'https://5e.tools/img/MM/'+monster.results[i].name+'.png'; /* Chapuza, pero la API no me proporciona imagenes */
+        td.colSpan=2;
+        td.rowSpan=4;
+        td.appendChild(img);
+
+        /*SUB ATRIBUTOS
+            * CA
+            * TIPO CA
+            * HP
+            * DADOS HP
+            * MOVEMENT
+        * */
+
+        var tr = document.createElement("tr");
+        tr.className="ca";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>Armor Class</a>  "+monster.results[i].armor_class+" ("+monster.results[i].armor_desc+")";
+        td.colSpan=2;
+        tr.appendChild(td);
+
+        var tr = document.createElement("tr");
+        tr.className="hp";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>Hit Points</a> "+monster.results[i].hit_points+" ("+monster.results[i].hit_dice+")";
+        td.colSpan=2;
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+
+        var tr = document.createElement("tr");
+        tr.className="movement";
+        tabla.appendChild(tr);
+
+        let movimiento_bicho ="<a>Speed</a> "
+        /*Checkeo de los distintos de movimientos, no todos los monstruos vuelan, o no todos nadan.*/
+        if (monster.results[i].speed.walk){movimiento_bicho+=(monster.results[i].speed.walk+ " ft");}
+        if (monster.results[i].speed.fly){movimiento_bicho+=(", Fly "+monster.results[i].speed.fly+ " ft");}
+        if (monster.results[i].speed.swim){movimiento_bicho+=(", Swin "+monster.results[i].speed.swim+ " ft ");}
+        td.innerHTML = movimiento_bicho;
+        td.colSpan=2;
+        td.className="rayote";
+        tr.appendChild(td);
+
+        /*ATRIBUTOS
+            * STRENGHT
+            * DEXTERY
+            * CONSTITUTION
+            * INTELLIGENCE
+            * WISDOM
+            * CHARISMA
+        * */
+
+        var tr = document.createElement("tr");
+        tr.className="stats";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>STR</a>";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>DEX</a>";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>CON</a>";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>INT</a>";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>WIS</a>";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        td.innerHTML ="<a>CHA</a>";
+        tr.appendChild(td);
+
+
+        /*ATRIBUTOS --- Valor numérico + bono
+            * STRENGHT
+            * DEXTERY
+            * CONSTITUTION
+            * INTELLIGENCE
+            * WISDOM
+            * CHARISMA
+        * */
+
+        var tr = document.createElement("tr");
+        tr.className="stats-base-bonos";
+        tabla.appendChild(tr);
+
+        var td = document.createElement("td");
+        let fuerza = Math.trunc((monster.results[i].strength-10)/2);
+        td.innerHTML =monster.results[i].strength+" (+"+fuerza+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        let destreza = Math.trunc((monster.results[i].dexterity-10)/2);
+        td.innerHTML =monster.results[i].dexterity+" (+"+destreza+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+
+        var td = document.createElement("td");
+        let constitucion = Math.trunc((monster.results[i].constitution-10)/2);
+        td.innerHTML =monster.results[i].constitution+" (+"+constitucion+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        let inteligencia = Math.trunc((monster.results[i].intelligence-10)/2);
+        td.innerHTML =monster.results[i].intelligence+" (+"+inteligencia+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        let sabiduria = Math.trunc((monster.results[i].wisdom-10)/2);
+        td.innerHTML =monster.results[i].wisdom+" (+"+sabiduria+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+        var td = document.createElement("td");
+        let carisma = Math.trunc((monster.results[i].charisma-10)/2);
+        td.innerHTML =monster.results[i].charisma+" (+"+carisma+") ";
+        td.className="rayote";
+        tr.appendChild(td);
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
